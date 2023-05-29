8ae0123f4b8e0bded7c34209a366ba69eb372434
Hiago Macedo <hiago.m.a.macedo@gmail.com>
2023-05-29 14:50:34 -0300

    Criado a classe Motor

---
+++ lib/Motor/Motor.cpp
@@ -0,0 +1,52 @@
+#include <Arduino.h>
+#include "Motor.h"
+
+Motor::Motor() {}
+Motor::Motor(byte pino1, byte pino2, byte pino_enable)
+: _pino1 (pino1)
+, _pino2 (pino2)
+, _pinoEnable (pino_enable)
+{}
+
+
+void Motor::init()
+{
+  pinMode(_pino1, OUTPUT);
+  pinMode(_pino2, OUTPUT);
+  pinMode(_pinoEnable, OUTPUT);
+  analogWrite(_pinoEnable, 0);
+}
+
+void Motor::moverFrente(byte velocidade)
+{
+  analogWrite(this->_pinoEnable, velocidade);
+  digitalWrite(this->_pino1, HIGH);
+  digitalWrite(this->_pino2, LOW);
+}
+
+void Motor::moverTras(byte velocidade)
+{
+  analogWrite(this->_pinoEnable, velocidade);
+  digitalWrite(this->_pino2, HIGH);
+  digitalWrite(this->_pino1, LOW);
+}
+
+void Motor::parar()
+{
+  analogWrite(this->_pinoEnable, 0);
+  digitalWrite(this->_pino1, LOW);
+  digitalWrite(this->_pino2, LOW);
+}
+
+void Motor::setPino1(byte pino_1) { this->_pino1 = pino_1; }
+
+void Motor::setPino2(byte pino_2) { this->_pino2 = pino_2; }
+
+void Motor::setPinoEnable(byte pino_enable) { this->_pinoEnable = pino_enable; }
+
+void Motor::setPinos(byte pino_1, byte pino_2, byte pino_enable)
+{
+  setPino1(pino_1);
+  setPino2(pino_2);
+  setPinoEnable(pino_enable);
+}
---
+++ lib/Motor/Motor.h
@@ -0,0 +1,28 @@
+#ifndef Motor_h
+#define Motor_h
+
+#include <Arduino.h>
+
+class Motor
+{
+public:
+  Motor();
+  Motor(byte pino1, byte pino2, byte pino_enable);
+
+  void init();
+  void moverFrente(byte velocidade);
+  void moverTras(byte velocidade);
+  void parar();
+
+  void setPino1(byte pino_1);
+  void setPino2(byte pino_2);
+  void setPinoEnable(byte pino_enable);
+  void setPinos(byte pino1, byte pino2, byte pino_enable);
+
+private:
+  byte _pino1;
+  byte _pino2;
+  byte _pinoEnable;
+};
+
+#endif
