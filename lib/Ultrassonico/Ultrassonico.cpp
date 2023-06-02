a29a9494f691477a17b9741ebf73580375229888
Hiago Macedo <hiago.m.a.macedo@gmail.com>
2023-06-02 10:50:55 -0300

    Implementado o metodo scannerArea

--- lib/Ultrassonico/Ultrassonico.cpp
+++ lib/Ultrassonico/Ultrassonico.cpp
@@ -4,10 +4,9 @@
 
 Ultrassonico::Ultrassonico() {}
 
-Ultrassonico::Ultrassonico(byte pino_trigger, byte pino_echo, int distancia_max)
+Ultrassonico::Ultrassonico(byte pino_trigger, byte pino_echo)
   : _pinoTrigger (pino_trigger)
   , _pinoEcho (pino_echo)
-  , _velocidadeSom (0.0343)
 {}
 
 
@@ -28,38 +27,89 @@ bool Ultrassonico::checarObjeto(int distanciaMax)
   return (ping() <= distanciaMax);
 }
 
-/**
- * @fn      ping
- * @result  Retorna a distancia do objeto em cm (Centimetro)
- */
-float Ultrassonico::ping()
+float static total_ping;
+int static qtd_ping;
+int Ultrassonico::ping()
 {
-  digitalWrite(_pinoTrigger, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(_pinoTrigger, LOW);
+  total_ping = 0;
+  for (qtd_ping = 0; qtd_ping < 5; qtd_ping++){
+    digitalWrite(_pinoTrigger, LOW);
+    delayMicroseconds(2);
+    digitalWrite(_pinoTrigger, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(_pinoTrigger, LOW);
 
-  _duracao = pulseIn(_pinoEcho, HIGH);
-
-  return (_duracao * 0.01715); // 0.01715 = Velocidade do som em cm/μs dividido por 2
+    _duracao = pulseIn(_pinoEcho, HIGH);
+    total_ping += (_duracao * 0.01715); /* 0.01715 = Velocidade do som em cm/μs dividido por 2 */
+  }
+  return (total_ping/qtd_ping);
 }
 
 
-int Ultrassonico::scannerArea(int distanciaMax)
+// int Ultrassonico::scannerArea(int distanciaMax)
+// {
+//   _servo.mover(0);
+//   if (!checarObjeto(distanciaMax)) {
+//     return 1;
+//   }
+
+//   _servo.mover(90);
+//   if (!checarObjeto(distanciaMax)) {
+//     return 2;
+//   }
+
+//   _servo.mover(180);
+//   if (!checarObjeto(distanciaMax)) {
+//     return 3;
+//   }
+
+//   return 0;
+// }
+
+typedef struct Distancia {
+  unsigned int grau;
+  float distancia;
+} Distancia;
+Distancia distancia[3];
+
+static unsigned int grauDistanciaMax;
+static float distanciaMax;
+static float distanciaAtual;
+// Distancia static d2;
+int Ultrassonico::scannerArea()
 {
   _servo.mover(0);
-  if (!checarObjeto(distanciaMax)) {
-    return 1;
-  }
+  grauDistanciaMax = 0;
+  distanciaMax = ping();
+  Serial.print("Distancia(0) = ");
+  Serial.println(distanciaMax);
+  // distancia[0].grau = 0;
+  // distancia[0].distancia = ping();
 
   _servo.mover(90);
-  if (!checarObjeto(distanciaMax)) {
-    return 2;
+  // distancia[1].grau = 90;
+  // distancia[1].distancia = ping();
+  distanciaAtual = ping();
+  Serial.print("Distancia(90) = ");
+  Serial.println(distanciaAtual);
+  if (distanciaAtual > distanciaMax) {
+    distanciaMax = distanciaAtual;
+    grauDistanciaMax = 90;
   }
 
   _servo.mover(180);
-  if (!checarObjeto(distanciaMax)) {
-    return 3;
+  // distancia[2].grau = 180;
+  // distancia[2].distancia = ping();
+  distanciaAtual = ping();
+  Serial.print("Distancia(180) = ");
+  Serial.println(distanciaAtual);
+  if (distanciaAtual > distanciaMax) {
+    distanciaMax = distanciaAtual;
+    grauDistanciaMax = 180;
   }
-
-  return 0;
+  
+  Serial.print("Grau: ");
+  Serial.println(grauDistanciaMax);
+  return grauDistanciaMax;
+  // return 0;
 }
--- lib/Ultrassonico/Ultrassonico.h
+++ lib/Ultrassonico/Ultrassonico.h
@@ -9,19 +9,17 @@ class Ultrassonico
 {
 public:
   Ultrassonico();
-  Ultrassonico(byte pino_trigger, byte pino_echo, int distancia_max);
+  Ultrassonico(byte pino_trigger, byte pino_echo);
   void init(byte pino_servo);
   bool checarObjeto(int distanciaMax);
-  float ping();
-  int scannerArea(int distanciaMax);
+  int ping();
+  int scannerArea();
 
 private:
   byte _pinoTrigger;
   byte _pinoEcho;
-  // float _distanciaMax;  // Em centimetros
-  float _distanciaAtual;
   float _duracao;
-  float _velocidadeSom; // Em cm/μs (Centimetro por microsegundo)
+  float distancias[3];
   MeuServo _servo;
   // NewPing* _sonar;
 };
